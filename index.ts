7e38112cefa79bf3c08068a2a8c5938a357ec58b
fareed <m.fareed.1411@gmail.com>
2025-11-19 17:30:43 +0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import routes from "./src/routes/index.ts";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+if (!process.env.DB_URL) {
+  throw new Error("❌ DB_URL is not defined in environment variables.");
+}
+
+mongoose.connect(process.env.DB_URL);
+mongoose.connection.on("connected", () => {
+  console.log("✅ MongoDB is connected.");
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT:${PORT}`);
+});
